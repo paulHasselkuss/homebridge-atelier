fe058279970fffd9a1716477e778d63ab0e72572
paul.hasselkuss <git@hasselkuss.com>
2024-10-30 17:01:43 +0100

    Clean-up

--- src/atelier/cmdHandler.ts
+++ src/atelier/cmdHandler.ts
@@ -8,9 +8,9 @@ export class CmdHandler {
 
   private static BAUD_RATE = 300;
   private static STAT_DELIMITER = '\r\n';
-  private static CMD_DELAY = 500;
-  private static XSTAT_DELAY = 3000;
-  private static STATE_DELAY = 3000;
+  private static DELAY_CMD = .5 * 1000;
+  private static DELAY_XSTAT = 3 * 1000;
+  private static DISPLAY_REFRESH_WINDOW = 3 * 1000;
 
   private readonly port: SerialPort | SerialPortMock;
   private readonly queue = new Queue<{ cmd: Cmd; func: () => void }>();
@@ -46,7 +46,7 @@ export class CmdHandler {
     });
   }
 
-  enqueCmd(cmd: Cmd, callback: () => void = () => { }): void {
+  enqueCmd(cmd: Cmd, callback = () => { }): void {
     this.log.debug('Cmd enqueued ', cmd);
     this.queue.enqueue({
       cmd: cmd,
@@ -73,7 +73,7 @@ export class CmdHandler {
     return false;
   }
 
-  wasStateUpdated(): boolean {
+  wasDeviceUpdated(): boolean {
     const first = this.history.peekFirst();
     const second = this.history.peekLast();
     const cmds = [Cmd.VOLUME_DOWN, Cmd.VOLUME_UP];
@@ -87,7 +87,7 @@ export class CmdHandler {
       return false;
     }
     //state was updated only if first and second where send within 3s
-    return first.timestamp.getTime() - second.timestamp.getTime() < CmdHandler.STATE_DELAY;
+    return first.timestamp.getTime() - second.timestamp.getTime() < CmdHandler.DISPLAY_REFRESH_WINDOW;
   }
 
   enqueueVolChange(target: number, current: number = this.state.volume, up: Cmd = Cmd.VOLUME_UP, down: Cmd = Cmd.VOLUME_DOWN): void {
@@ -106,7 +106,7 @@ export class CmdHandler {
       if (current !== this.targetVolume) {
         const cmd = current < this.targetVolume ? up : down;
         this.write(cmd, () => cmd === up ? current++ : current--);
-        setTimeout(repeatingTask, CmdHandler.CMD_DELAY);
+        setTimeout(repeatingTask, CmdHandler.DELAY_CMD);
       } else {
         this.log.debug('Volume change to %s finished.', this.targetVolume);
         this.targetVolume = -1;
@@ -137,8 +137,6 @@ export class CmdHandler {
         timestamp: new Date(),
       });
 
-      callback();
-
       // Set to offline if we do not receive a reponse
       if (cmd === Cmd.XMIT_STAT) {
         const then = this.state.timestamp;
@@ -156,7 +154,7 @@ export class CmdHandler {
       setTimeout(() => {
         this.isProcessing = false;
         this.processQueue();
-      }, cmd === Cmd.XMIT_STAT ? CmdHandler.XSTAT_DELAY : CmdHandler.CMD_DELAY);
+      }, cmd === Cmd.XMIT_STAT ? CmdHandler.DELAY_XSTAT : CmdHandler.DELAY_CMD);
     });
   }
 }
--- src/atelier/device.ts
+++ src/atelier/device.ts
@@ -22,40 +22,28 @@ export class Device {
   }
 
   tooglePower(on: boolean) {
-    const diff = Date.now() - this.stateRequested.getTime();
-    this.log.debug('Status was last updated %s ms ago (treshold is %s).', diff, Device.STATUS_UPDATE_THRESHOLD);
-    this.log.debug('Current status is: \n', JSON.stringify(this.state, null, 2));
-
-    const callback = () => {
-      if (this.getState().isOn !== on) {
+    this.refreshState(() => {
+      if (this.state.isOn !== on) {
         this.cmdHandler.enqueCmd(Cmd.ON_OFF, () => this.state.isOn = on);
       }
-    };
-
-    if (diff > Device.STATUS_UPDATE_THRESHOLD) {
-      this.log.debug('Updating status...');
-      this.cmdHandler.enqueCmd(Cmd.XMIT_STAT, callback);
-      this.stateRequested = new Date();
-    } else {
-      callback();
-    }
+    });
   }
 
   toogleMute(on: boolean) {
-    if (this.getState().isMute !== on) {
+    if (this.state.isMute !== on) {
       this.cmdHandler.enqueCmd(Cmd.MUTE, () => this.state.isMute = on);
     }
   }
 
   toogleLoudness(on: boolean) {
-    if (this.getState().isLoudness !== on) {
+    if (this.state.isLoudness !== on) {
       this.cmdHandler.enqueCmd(Cmd.LOUDNESS, () => this.state.isLoudness = on);
     }
   }
 
   increaseVolume() {
     this.cmdHandler.enqueCmd(Cmd.VOLUME_UP, () => {
-      if (this.cmdHandler.wasStateUpdated() && this.getState().volume < 100) {
+      if (this.cmdHandler.wasDeviceUpdated() && this.state.volume < 100) {
         this.state.volume++;
       }
     });
@@ -63,7 +51,7 @@ export class Device {
 
   decreaseVolume() {
     this.cmdHandler.enqueCmd(Cmd.VOLUME_DOWN, () => {
-      if (this.cmdHandler.wasStateUpdated() && this.getState().volume > 0) {
+      if (this.cmdHandler.wasDeviceUpdated() && this.state.volume > 0) {
         this.state.volume--;
       }
     });
@@ -71,28 +59,20 @@ export class Device {
 
   setVolume(target: number) {
     assert(target >= 0 && target <= 100, 'Volume target must be between 0 and 100.');
-    if (this.getState().volume !== target) {
+    if (this.state.volume !== target) {
       this.cmdHandler.enqueueVolChange(target);
       this.state.volume = target;
     }
   }
 
   setInput(to: Input) {
-    if (this.getState().inputSource !== to) {
+    if (this.state.inputSource !== to) {
       this.cmdHandler.enqueCmd(this.toCmd(to), () => this.state.inputSource = to);
     }
   }
 
   getState() {
-    const diff = Date.now() - this.stateRequested.getTime();
-    this.log.debug('Status was last updated %s ms ago (treshold is %s).', diff, Device.STATUS_UPDATE_THRESHOLD);
-    this.log.debug('Current status is: \n', JSON.stringify(this.state, null, 2));
-
-    if (diff > Device.STATUS_UPDATE_THRESHOLD && !this.cmdHandler.isEnqueued(Cmd.XMIT_STAT)) {
-      this.log.debug('Updating status...');
-      this.cmdHandler.enqueCmd(Cmd.XMIT_STAT);
-      this.stateRequested = new Date();
-    }
+    this.refreshState();
     return this.state;
   }
 
@@ -126,6 +106,23 @@ export class Device {
       case Input.Phono: return Cmd.INPUT_PHONO;
       case Input.TV: return Cmd.INPUT_TV;
       default: throw Error('Unexpected Input.');
+    }
+  }
+
+  // when callback is run, we know if the device is online or offline
+  // either, the status is still recent or XMIT_STAT is enqueued
+  // callback is called about 500ms after XMIT_STAT was written
+  private refreshState(callback = () => { }) {
+    const diff = Date.now() - this.stateRequested.getTime();
+    this.log.debug('Status was last updated %s ms ago (treshold is %s).', diff, Device.STATUS_UPDATE_THRESHOLD);
+    this.log.debug('Current status is: \n', JSON.stringify(this.state, null, 2));
+
+    if (diff > Device.STATUS_UPDATE_THRESHOLD) {
+      this.log.debug('Updating status...');
+      this.cmdHandler.enqueCmd(Cmd.XMIT_STAT, callback);
+      this.stateRequested = new Date();
+    } else {
+      callback();
     }
   }
 }
